278690656a520118ba5a544ee30638d11ec67232
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 22:22:09 -0700

    hegiht

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,8 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_side, right_side;
+
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_height(tree->left));
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+
+	if (left_side > right_side)
+		return (left_side + 1);
+
+	return (right_side + 1);
 }
