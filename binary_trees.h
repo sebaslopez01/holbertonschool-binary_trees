f48e05ee0ae70e80cf517977ad99155c281c26e5
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 20:42:14 -0700

    Fixed n

--- binary_trees.h
+++ binary_trees.h
@@ -14,18 +14,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_trees_s *parent;
 	struct binary_trees_s *left;
 	struct binary_trees_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+} binary_tree_t;
 
 
 /* Prototypes */
