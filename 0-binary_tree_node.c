4e22105dd9af35fc6b9a2bee696bea764b0233ff
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 20:45:14 -0700

    Betty styles

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
