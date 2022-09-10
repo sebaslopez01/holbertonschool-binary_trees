a253e65c84523d49f75a639be9d8f050434a5f16
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 20:53:46 -0700

    Parent fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,8 +23,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	if (parent->left != NULL)
 		new_node->left = parent->left;
-	new_node->parent = parent->left;
+	new_node->parent = parent;
 	new_node->right = NULL;
+	parent->left = new_node;
 
 	return (new_node);
 }
