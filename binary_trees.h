d0d7685d06258ac34600a55441f163647f707066
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 20:44:30 -0700

    Def fixed

--- binary_trees.h
+++ binary_trees.h
@@ -14,13 +14,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
-	struct binary_trees_s *parent;
-	struct binary_trees_s *left;
-	struct binary_trees_s *right;
-} binary_tree_t;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 
 /* Prototypes */
