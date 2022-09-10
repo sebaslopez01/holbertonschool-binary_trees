3ad237f7b914631f20369fa93718d99aaa9489d3
Sebastian <sebaslopez.2002@gmail.com>
2022-09-09 22:09:10 -0700

    hegiht

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree
+ *
+ * Return: The height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_height(tree->left) + binary_tree_height(tree->right));
+}
