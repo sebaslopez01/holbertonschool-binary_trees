59ab036b3455849a363900b2783bff329861fc83
Sebastian <sebaslopez.2002@gmail.com>
2022-09-10 19:13:02 -0700

    Fixed typo

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,8 @@ size_t binary_tree_height_helper(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_side = binary_tree_height(tree->left);
-	right_side = binary_tree_height(tree->right);
+	left_side = binary_tree_height_helper(tree->left);
+	right_side = binary_tree_height_helper(tree->right);
 
 	if (left_side > right_side)
 		return (left_side + 1);
