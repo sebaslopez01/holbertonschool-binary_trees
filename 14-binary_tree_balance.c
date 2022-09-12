383fe848fbf51bd6f9a7e58f7378bdaf8a061035
Sebastian <sebaslopez.2002@gmail.com>
2022-09-11 17:21:37 -0700

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,40 +2,25 @@
 
 
 /**
- * binary_tree_height_helper - Measures the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree
  * @tree: A pointer to the root node of the tree
  *
  * Return: The height of the full tree
  */
-size_t binary_tree_height_helper(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_side, right_side;
 
 	if (tree == NULL)
 		return (0);
 
-	left_side = binary_tree_height_helper(tree->left);
-	right_side = binary_tree_height_helper(tree->right);
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
 
 	if (left_side > right_side)
 		return (left_side + 1);
 
 	return (right_side + 1);
-}
-
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: A pointer to the root node of the tree
- *
- * Return: The height of the tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (binary_tree_height_helper(tree) - 1);
 }
 
 
@@ -50,5 +35,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height_helper(tree->left) - binary_tree_height_helper(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
